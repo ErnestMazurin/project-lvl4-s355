737062d38e364a1dfd38cb2d0670a53436239e3d
ErnestMazurin <chegevara001@gmail.com>
2019-01-09 09:22:44 +0300

    not worked yet

---
+++ src/components/AddChannelModal.jsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import { Button, Modal } from 'react-bootstrap';
+import { Field } from 'redux-form';
+import connect from '../connect';
+import reduxForm from '../reduxForm';
+
+const mapStateToProps = ({ newChannelRequestStatus }) => ({ newChannelRequestStatus });
+
+@connect(mapStateToProps)
+@reduxForm({ form: 'newChannelName' })
+class AddChannelModal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: false,
+    };
+  }
+
+  handleShow = (event) => {
+    event.preventDefault();
+    this.setState({ show: true });
+  };
+
+  handleClose = (event) => {
+    event.preventDefault();
+    this.close();
+  };
+
+  close = () => {
+    this.setState({ show: false });
+  };
+
+  submit = ({ name }) => {
+    const { sendNewChannel, reset } = this.props;
+    return sendNewChannel(name).then(() => {
+      reset();
+      this.close();
+    });
+  };
+
+  render() {
+    const { show } = this.state;
+    const { handleSubmit, submitting, newChannelRequestStatus } = this.props;
+    return (
+      <span>
+        <a className="badge badge-secondary ml-1" onClick={this.handleShow} href="">New</a>
+        <Modal show={show}>
+          <form onSubmit={handleSubmit(this.submit)}>
+            <Modal.Header>
+              <Modal.Title>New channel</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <div className="input-group">
+                <Field
+                  name="name"
+                  required
+                  component="input"
+                  type="text"
+                  placeholder="Enter channel name ..."
+                  className="form-control"
+                  autoComplete="off"
+                />
+              </div>
+              {newChannelRequestStatus === 'failure' ? <div className="alert alert-danger">Network error</div> : null}
+            </Modal.Body>
+            <Modal.Footer>
+              <Button onClick={this.handleClose} disabled={submitting}>Close</Button>
+              <Button type="submit" className="primary" disabled={submitting}>Save channel</Button>
+            </Modal.Footer>
+          </form>
+        </Modal>
+      </span>
+    );
+  }
+}
+
+export default AddChannelModal;
--- src/components/Channels.jsx
+++ src/components/Channels.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import connect from '../connect';
+import AddChannelModal from './AddChannelModal';
 
 const mapStateToProps = ({ channels, currentChannelId }) => ({ channels, currentChannelId });
 
@@ -7,19 +8,28 @@ const mapStateToProps = ({ channels, currentChannelId }) => ({ channels, current
 class Channels extends React.Component {
   changeChannelHandler = id => (event) => {
     event.preventDefault();
-    this.props.changeChannel({ id });
+    const { changeChannel } = this.props;
+    changeChannel({ id });
   };
 
   render() {
     const { channels, currentChannelId } = this.props;
     return (
       <div>
-        <div className="mt-4 font-italic">Channels:</div>
+        <div className="mt-4 font-italic">
+          Channels:
+          <AddChannelModal />
+        </div>
         <ul className="list-group">
           {channels.map(({ id, name }) => {
             const className = `list-group-item list-group-item-action pl-1 ${id === currentChannelId ? 'active' : ''}`;
             return (
-              <a key={id} className={className} onClick={this.changeChannelHandler(id)} href="">
+              <a
+                key={id}
+                className={className}
+                onClick={this.changeChannelHandler(id)}
+                href=""
+              >
                 {`# ${name}`}
               </a>
             );
