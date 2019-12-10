de58f827d61eb37df9c0d0114ca20f19f4436864
Ernest <chegevara001@gmail.com>
2019-12-10 14:20:21 +0300

    update koa-pug

--- server/index.js
+++ server/index.js
@@ -42,11 +42,8 @@ export default () => {
   app.use(koaLogger());
   const pug = new Pug({
     viewPath: path.join(__dirname, '..', 'views'),
-    debug: true,
-    pretty: true,
-    compileDebug: true,
     locals: [],
-    noCache: process.env.NODE_ENV !== 'production',
+    cache: process.env.NODE_ENV === 'production',
     basedir: path.join(__dirname, 'views'),
     helperPath: [
       { _ },
